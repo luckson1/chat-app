08e79012cd6eec02b4186f54a09a7b83561d7dc9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-30 04:01:07 +0300

    feature: add a page to manage authorisation

---
+++ src/pages/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  //check if user is login in
+  const userDatFromStorage =sessionStorage.getItem("userData")  ? JSON.parse(sessionStorage.getItem("userData")): undefined;
+
+  const userLogin= userDatFromStorage?.userId
+  return userLogin? children: <Navigate to='/' />
+};
+export default ProtectedRoute;
