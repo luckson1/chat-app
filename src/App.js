be29bde99d93a15a919242330ab6c38f085e386d
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-30 04:05:44 +0300

    feature: add authorisation to app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import { ChatRoom } from "./pages/ChatRoom";
-
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route exact path="/" element={<Home />} />
 
-        <Route exact path="/chat-room" element={<ChatRoom />} />
-
+        <Route
+          exact
+          path="/chat-room"
+          element={
+            <ProtectedRoute>
+              <ChatRoom />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
